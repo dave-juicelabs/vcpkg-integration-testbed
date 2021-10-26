cca03ccf9854c90204ce6fda972e1a195368c1b2
David McCloskey <davmcclo@gmail.com>
2021-10-26 10:06:41 -0500

    Fixing archives directory creation.

--- vcpkg.ps1
+++ vcpkg.ps1
@@ -10,13 +10,13 @@ param (
 Git-Clone -Repository 'https://github.com/microsoft/vcpkg.git' -Branch 'master' -Name 'vcpkg' -Path $PSScriptRoot -RemoveIfInvalid -ForceSet
 
 # Set the environment variable for binary caching location
-$ArchivesPath = [System.IO.Path]::Join($PSScriptRoot, 'archives')
+$ArchivesPath = [System.IO.Path]::Join($PSScriptRoot, 'vcpkg', 'archives')
 $env:VCPKG_DEFAULT_BINARY_CACHE = $ArchivesPath
 
 # Make sure the archives folder is available
 if(!(Test-Path -Path $ArchivesPath))
 {
-    New-Item -Name $ArchivesPath -Type Directory
+    New-Item -Path $ArchivesPath -Type Directory -ErrorAction Stop
 }
 
 # Bootstrap vcpkg to ensure it is correct
